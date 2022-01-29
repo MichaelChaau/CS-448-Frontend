e467612634c498b78fe92ef540dabf659dd7ec38
Vien Hua <vhua@worcester.edu>
2022-01-29 18:32:53 -0500

    initial commit, implement your refactor code in this branch

---
+++ src/components/id-input.vue
@@ -0,0 +1,13 @@
+<!-- Implement the id-input here -->
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/register-form.vue
@@ -0,0 +1,13 @@
+<!-- Implement the registration form here -->
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
