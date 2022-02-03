97c14005f3c0198c412a9f680bb7309ac442d806
Brendan Lai <blai1@worcester.edu>
2022-02-03 13:19:58 -0500

    id-input converted to vue

--- src/components/id-input.vue
+++ src/components/id-input.vue
@@ -1,9 +1,69 @@
 <!-- Implement the id-input here -->
 <template>
-    <h1>Hello World</h1>
+    <head>
+    <meta charset="utf-8">
+    <title>ID Input</title>
+    <link rel="stylesheet" href="style.css">
+    <script type="application/javascript" src="https://unpkg.com/vue@next" defer></script>
+  </head>
+  <body>
+    <h1>Enter ID</h1>
+    <main id="app">
+        <p>Try typing spaces, punctuation, or special characters into each box to see the difference made by the key handler.</p>
+            <input v-model="idnum" @keypress.prevent @keyup=idnumChange></input> <button :disabled=!submit>Submit</button>
+      </main>
+  </body>
 </template>
 
 <script>
+function isDigit(key) {
+    return key >= '0' && key <= '9';
+}
+
+export default{
+    data() {
+      return {
+          idnum: '',
+          idInputState: 'manual',
+          swipeZeros: 0,
+          submit: false,
+      }
+  },
+
+    methods: {
+      idnumChange(e) {
+        // enter swipe mode if we get a semicolon as the first character
+        if (this.idInputState === 'manual' && this.idnum.length === 0 && e.key === ';') {
+            this.idInputState = 'swipe';
+            this.swipeZeros = 0;
+        } 
+        // manual mode: accept only digits until we get 7
+        else if (this.idInputState === 'manual') {
+            this.accept7Digits(e.key);
+        } 
+        // swipe mode: watch for 4 zeros, then accept 7 digits
+        else if (this.idInputState === 'swipe') {
+            if (this.idnum.length === 0 && this.swipeZeros < 4) {
+                if (e.key === '0') {
+                    this.swipeZeros++;
+                } else {
+                    this.idInputState = 'manual'
+                }
+            } else {
+            this.accept7Digits(e.key);
+            }
+        }
+        },
+      accept7Digits(key) {
+        if (this.idnum.length < 7 && isDigit(key)) {
+            this.idnum = this.idnum + key;
+        if (this.idnum.length === 7) {
+          this.submit = true;
+        }
+      }
+    }
+  }
+}
 
 </script>
 
